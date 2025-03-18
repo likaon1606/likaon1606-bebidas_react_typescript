f0b55d3f2f62def5d2eb5625f35fc3cf66025c4e
likaon1606 <ariel.fuentesgarcia@gmail.com>
2025-03-17 21:03:37 -0600

    favorites finish

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -4,18 +4,32 @@ import { Recipe } from '../types'
 export type FavoritesSliceType = {
   favorites: Recipe[]
   handleClickFavorite: (recipe: Recipe) => void 
+  favoriteExists: (id: Recipe['idDrink']) => boolean
+  loadFromStorage: () => void
 }
 
 export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get) => ({
   favorites: [],
   handleClickFavorite: (recipe) => {
-    if (get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)) {
+    if (get().favoriteExists(recipe.idDrink)) {
       set((state) => ({
         favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink )
       }))
     } else {
       set({
         favorites: [...get().favorites, recipe]
+      })
+    }
+    localStorage.setItem('favorites', JSON.stringify(get().favorites))
+  },
+  favoriteExists: (id) => {
+    return get().favorites.some(favorite => favorite.idDrink === id)
+  },
+  loadFromStorage: () => {
+    const storedFavorites = localStorage.getItem('favorites')
+    if (storedFavorites) {
+      set({
+        favorites: JSON.parse(storedFavorites)
       })
     }
   }
