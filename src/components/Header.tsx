917f98b04f470500f0dec9aea69a117843db730d
likaon1606 <ariel.fuentesgarcia@gmail.com>
2025-03-21 16:16:05 -0600

    bg image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
   }
 
   return (
-    <header className={ isHome ? 'bg-[url("bg.jpg")] bg-center bg-cover' : 'bg-slate-800' }>
+    <header className={ isHome ? 'background__home' : 'bg-slate-800' }>
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
           <div>
